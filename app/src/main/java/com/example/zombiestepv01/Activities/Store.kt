a2d81bc774fdedbb1e83a368fc04652d24520df8
Francesco-hub <56824952+Francesco-hub@users.noreply.github.com>
2021-05-25 21:17:48 +0200

    Made stepcoins appear in menu

--- app/src/main/java/com/example/zombiestepv01/Activities/Store.kt
+++ app/src/main/java/com/example/zombiestepv01/Activities/Store.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.zombiestepv01.Model.BEUser
 import com.example.zombiestepv01.R
@@ -19,17 +20,20 @@ class Store : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListene
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_store)
+        var extras: Bundle = intent.extras!!
+        user = extras.getSerializable("loggedUser") as BEUser
         setSupportActionBar(toolbar)
-
         val toggle = ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open,R.string.close)
         toggle.isDrawerIndicatorEnabled = true
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         imageButton.setOnClickListener{v-> drawerLayout.open()}
         var navigationView : NavigationView = findViewById(R.id.nav_menu)
+        val headerView = navigationView.getHeaderView(0)
+        val txt_stepcoins = headerView.findViewById(R.id.stepcoins) as TextView
+        txt_stepcoins.text = "Stepcoins: ${user.stepCoins}"
         navigationView.setNavigationItemSelectedListener(this)
-        var extras: Bundle = intent.extras!!
-        user = extras.getSerializable("loggedUser") as BEUser
+
     }
 
     override fun onBackPressed() {

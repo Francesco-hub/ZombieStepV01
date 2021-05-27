5611735f8017ee59003dea6cbde49a926b1b95ef
Francesco-hub <56824952+Francesco-hub@users.noreply.github.com>
2021-05-27 13:44:27 +0200

    Custom Camera

--- app/src/main/java/com/example/zombiestepv01/Activities/CameraActivity.kt
+++ app/src/main/java/com/example/zombiestepv01/Activities/CameraActivity.kt
@@ -30,7 +30,6 @@ class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
-
         // Request camera permissions
         if (allPermissionsGranted()) {
             startCamera()
